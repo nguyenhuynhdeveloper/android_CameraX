26c7d87d75530c3bc1d3ce10ab9f29c88f1df3a8
nguyenhuynhdeveloper <nguyenhuynh.developer@gamil.com>
2024-09-13 00:42:14 +0700

    add implement feature Zoom add code not run ok

--- app/src/main/java/com/example/android_camerax/MainActivity.java
+++ app/src/main/java/com/example/android_camerax/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     private float maxZoom;
     private Rect zoomRect;
 
+
+    private String TAG = "Zoom";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -165,20 +167,64 @@ public class MainActivity extends AppCompatActivity {
         try {
             CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
             CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
+
             Rect activeRect = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE);
-            int minW = (int) (activeRect.width() / maxZoom);
-            int minH = (int) (activeRect.height() / maxZoom);
+            float maxZoom = characteristics.get(CameraCharacteristics.SCALER_AVAILABLE_MAX_DIGITAL_ZOOM);
+
+            Log.d(TAG, "activeRect: "+activeRect);
+            Log.d(TAG, "activeRect width: "+activeRect.width());
+            Log.d(TAG, "activeRect height: "+activeRect.height());
+            Log.d(TAG, "maxZoom: "+maxZoom);
+
+            // Tính toán lại vùng crop
+            int minW = (int) Math.floor(activeRect.width() / maxZoom);
+            int minH = (int) Math.floor(activeRect.height() / maxZoom);
+
             int difW = activeRect.width() - minW;
             int difH = activeRect.height() - minH;
+
             int cropW = difW * zoomLevel / 100;
             int cropH = difH * zoomLevel / 100;
-            zoomRect = new Rect(cropW, cropH, activeRect.width() - cropW, activeRect.height() - cropH);
+
+            // Đảm bảo vùng crop không vượt quá giới hạn
+            cropW = Math.min(cropW, difW);
+            cropH = Math.min(cropH, difH);
+
+            Log.d(TAG, "minW: "+minW);
+            Log.d(TAG, "minH: "+minH);
+            Log.d(TAG, "difW: "+difW);
+            Log.d(TAG, "difH: "+difH);
+            Log.d(TAG, "cropW: "+cropW);
+            Log.d(TAG, "cropH: "+cropH);
+
+
+            // Tạo vùng zoom mới
+            Rect zoomRect = new Rect(cropW, cropH, activeRect.width() - cropW, activeRect.height() - cropH);
+
+
+//// Kích thước của cảm biến camera
+//            int sensorWidth = activeRect.width();
+//            int sensorHeight = activeRect.height();
+//
+//// Tính toán kích thước cho zoomLevel
+//            int cropWidth = (int) (sensorWidth / zoomLevel);
+//            int cropHeight = (int) (sensorHeight / zoomLevel);
+//
+//// Tính toán điểm bắt đầu của vùng cắt (để giữ hình ảnh ở giữa)
+//            int cropLeft = (sensorWidth - cropWidth) / 2;
+//            int cropTop = (sensorHeight - cropHeight) / 2;
+//
+//// Tạo Rect mới để set vào SCALER_CROP_REGION
+//            Rect zoomRect = new Rect(cropLeft, cropTop, cropLeft + cropWidth, cropTop + cropHeight);
+
+            Log.d(TAG, "zoomRect end: "+zoomRect);
             captureRequestBuilder.set(CaptureRequest.SCALER_CROP_REGION, zoomRect);
             cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
